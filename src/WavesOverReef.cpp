5bdcc2b3a9e9df7bb1f10ab670aaa96146988de2
Tully <tully@be-lumino.us>
2019-01-27 16:24:35 -0600

    Break output symmetry

--- src/WavesOverReef.cpp
+++ src/WavesOverReef.cpp
@@ -30,18 +30,18 @@ void WavesOverReef::draw(const DrawState &state) {
     for (uint16_t pos = 0; pos < LED::Array0::Length; pos++)
     {
         fract8 bgPos = fractOf(pos, BGPaletteSpanPx);
-        CRGB bgColor = ColorFromPalette(bgPal, bgPos + bgTimePhaseOffset, bgVal);
-
         fract8 fg1Pos = fractOf(pos, FG1PaletteSpanPx);
         fract8 fg2Pos = fractOf(pos, FG2PaletteSpanPx);
 
-        LED::Array0::Buffer[pos] = bgColor +
+        LED::Array0::Buffer[pos] = 
+            ColorFromPalette(bgPal, bgPos + bgTimePhaseOffset, bgVal) +
             ColorFromPalette(raysPal, fg1Pos + fg1TimePhaseOffset, fg1Val) +
             ColorFromPalette(raysPal, fg2Pos - fg2TimePhaseOffset, fg2Val);
 
-        LED::Array1::Buffer[pos] = bgColor +
-            ColorFromPalette(raysPal, fg1Pos + fg1TimePhaseOffset, fg1Val) +
-            ColorFromPalette(raysPal, fg2Pos - fg2TimePhaseOffset, fg2Val);
+        LED::Array1::Buffer[pos] = 
+            ColorFromPalette(bgPal, bgPos + bgTimePhaseOffset + BGPaletteSpanPx/2, bgVal) +
+            ColorFromPalette(raysPal, fg1Pos + fg1TimePhaseOffset + FG1PaletteSpanPx/3, fg1Val) +
+            ColorFromPalette(raysPal, fg2Pos - fg2TimePhaseOffset + FG2PaletteSpanPx/3, fg2Val);
     }
 }
 
