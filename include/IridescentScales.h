02c26ddd94cca94e13f4d6c76141c291181597f7
Tully <tully@be-lumino.us>
2019-02-14 19:36:46 -0600

    Charge faster

--- include/IridescentScales.h
+++ include/IridescentScales.h
@@ -22,7 +22,7 @@ private:
     static constexpr int16_t BGPalettePeriodMs = 16000;
     static constexpr int16_t BGPaletteSpanPx = 256;
 
-    static constexpr int32_t FlashHoldMsPerPxLength = 80;
+    static constexpr int32_t FlashHoldMsPerPxLength = 20;
     static constexpr int32_t FlashTravelMsPerPx = 5;
 };
 
